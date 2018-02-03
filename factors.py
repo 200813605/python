445e8d080f8fc1f3a9e8fb3642a33e15ffbeb212
200813605 <200813605@myacg.org>
2018-02-03 17:12:54 +1300

    Finished factors.py

--- factors.py
+++ factors.py
@@ -1,1 +1,15 @@
-print "a,b,c..."
+factors = []
+
+def find_factors(number):
+    for i in range(1,number+1):
+        if number % i == 0:
+            factors.append(i)
+
+
+def print_factors():
+    print str.join(", ", (str(f) for f in factors))
+
+a = input("What number would you like the factors of? ")
+
+find_factors(a)
+print_factors()
