03a34273ecdbe7ad96a6ac5413f986c27ea0d7c6
200813605 <200813605@myacg.org>
2018-02-04 13:40:02 +1300

    made factors.py show amount of factors

--- factors.py
+++ factors.py
@@ -6,10 +6,11 @@ def find_factors(number):
             factors.append(i)
 
 
-def print_factors():
+def print_factors(number):
+    print"There are %d factors of %d"%(len(factors), number)
     print str.join(", ", (str(f) for f in factors))
 
 a = input("What number would you like the factors of? ")
 
 find_factors(a)
-print_factors()
+print_factors(a)
