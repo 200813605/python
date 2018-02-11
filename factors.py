2c6ed2fe17e760922223b8310c78a018d5bf5157
200813605 <200813605@myacg.org>
2018-02-11 18:15:57 +1300

    started to edit factors.py to support HCFs and LCMs

--- factors.py
+++ factors.py
@@ -1,4 +1,8 @@
+#variables
 factors = []
+
+'''
+#functions
 
 def find_factors(number):
     for i in range(1,number+1):
@@ -10,7 +14,24 @@ def print_factors(number):
     print"There are %d factors of %d"%(len(factors), number)
     print str.join(", ", (str(f) for f in factors))
 
-a = input("What number would you like the factors of? ")
+def ask_factors():
+    a = input("What number would you like the factors of? ")
+    global a
 
 find_factors(a)
 print_factors(a)
+
+'''
+answer = raw_input("What would you like to do (HCF,LCM,factors)? ")
+
+if answer.upper() == 'HCF':
+    print 'a'
+
+elif answer.upper() == 'LCM':  
+    print 'b'
+
+elif answer.lower() == 'factors':
+    print 'c'
+
+else:
+    print'd'
